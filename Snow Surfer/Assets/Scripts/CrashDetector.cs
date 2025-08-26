05df2ad900c002b9ee37fe91a30431b50eb49e43
itsChisomm <chisomc@hotmail.co.uk>
2025-08-26 13:10:34 +0100

    Added Delay and Finish/Crashing particles
    Added a delay for when the player reaches the finish line or crashes before restarting the level. Added particle systems to for visualization effects of when players finish or crash.

--- Snow Surfer/Assets/Scripts/CrashDetector.cs
+++ Snow Surfer/Assets/Scripts/CrashDetector.cs
@@ -3,12 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] private int sceneIndexToLoad;
+    [SerializeField] private float restartDelay = 1f;
+    [SerializeField] ParticleSystem crashParticles;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Floor");
         if (collision.gameObject.layer == layerIndex)
         {
-            SceneManager.LoadScene(0);
+            crashParticles.Play();
+            Invoke("ReloadScene", restartDelay);
+            //SceneManager.LoadScene(0);
         }
     }
+     void ReloadScene()
+    {
+        SceneManager.LoadScene(sceneIndexToLoad);
+    }
+
 }
+
--- Snow Surfer/Assets/Scripts/FinishLine.cs
+++ Snow Surfer/Assets/Scripts/FinishLine.cs
@@ -3,16 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] private int sceneIndexToLoad;
+    [SerializeField] private float restartDelay = 1f;
+    [SerializeField] ParticleSystem finishParticles;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Player");
 
         if (collision.gameObject.layer == layerIndex)
         {
-            SceneManager.LoadScene(0);
+            finishParticles.Play();
+            Invoke("ReloadScene", restartDelay);
+            //SceneManager.LoadScene(0);
             
         }
         
     }
 
+    void ReloadScene()
+    {
+               SceneManager.LoadScene(sceneIndexToLoad);
+    }
 }
