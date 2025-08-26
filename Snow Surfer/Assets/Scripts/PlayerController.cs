dee974ebdff8e013340228a50c1119716738f3a8
itsChisomm <chisomc@hotmail.co.uk>
2025-08-26 11:39:03 +0100

    Update PlayerController.cs
    Added the move input system to player controller

--- Snow Surfer/Assets/Scripts/PlayerController.cs
+++ Snow Surfer/Assets/Scripts/PlayerController.cs
@@ -3,15 +3,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+
+    InputAction moveAction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
+    {       
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveVector = moveAction.ReadValue<Vector2>(); // Read the value of the "Move" action
+        print(moveVector); // Print the move vector to the console
     }
 }
